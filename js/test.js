ad21425e4704b455a6a44263bb2499da69984e7b
Edisan Ibisi <edisan.ibisi@gmail.com>
2020-02-22 12:59:31 +0100

    started javascript

---
+++ js/test.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Link in Mitrovica."
+}
+
+function myResult () {
+    document.getElementById("mathResult").innerHTML = 5 + 6;
+    alert(2+2);
+}
+
+function statements() {
+    document.getElementById("doTheMath").innerHTML = c
+}
+
+
+var a = 10;
+var b = 12;
+var c = a + b;
+
+
+
